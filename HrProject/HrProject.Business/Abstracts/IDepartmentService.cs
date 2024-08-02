31fcd8e227551bdf4b4df8caf943e20683fe9dc4
URLeeo <mammadzadaaslan05@gmail.com>
2024-08-02 17:10:36 +0400

    addded department service interface

---
+++ HrProject/HrProject.Business/Abstracts/IDepartmentService.cs
@@ -0,0 +1,12 @@
+﻿using HrProject.Core.Entities;
+
+namespace HrProject.Business.Abstracts;
+
+public interface IDepartmentService
+{
+    void Create(string name);
+    void Update(int id);
+    void Delete(int id);
+    List<Department> GetAll();
+    Department GetById(int id);
+}
--- HrProject/HrProject.Core/Entities/Department.cs
+++ HrProject/HrProject.Core/Entities/Department.cs
@@ -2,13 +2,13 @@
 
 namespace HrProject.Core.Entities;
 
-public class Departament : IEntity<int>
+public class Department : IEntity<int>
 {
     public int Id { get; }
     public string Name { get; set; } = null!;
 
     private static int _id;
-    public Departament(string name)
+    public Department(string name)
     {
         Id=_id++;
         Name = name;
